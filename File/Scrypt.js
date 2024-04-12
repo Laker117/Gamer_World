3adbe4f37890dc9602c2793e809cfe1081e097b0
JLCamachoHernandez <joseluiscamachohernandez4@gmail.com>
2024-04-12 15:28:24 -0700

    CreateAccount page design (Non-functional)

--- File/Scrypt.js
+++ File/Scrypt.js
@@ -1,13 +1,38 @@
 //Login
-const nombre = document.getElementById("name")
-const password = document.getElementById("password")
-const button = document.getElementById("button")
+const nombreLogin = document.getElementById("nameLogin")
+const passwordLogin = document.getElementById("passwordLogin")
+const buttonLogin = document.getElementById("butonLogin")
 
-button.addEventListener('click', (e) =>{
+buttonLogin.addEventListener('click', (e) =>{
     e.preventDefault()
-    if(nombre.value.length<1){
+    if(nombreLogin.value.length<1){
         alert("Ingrese los datos correspondientes en la casilla de usuario")
-    } if(password.value.length<1){
+    } else if(passwordLogin.value.length<1){
         alert("Ingrese los datos correspondientes en la casilla de contraseña")
+    } else {
+        window.location="Index.html";
     }
 })
+
+/*CreateAccount
+const nombreCreateAccount = document.getElementById("nameCreateAccount")
+const userCreateAccount = document.getElementById("userCreateAccount")
+const emailCreateAccount = document.getElementById("emailCreateAccount")
+const passwordCreateAccount = document.getElementById("passwordCreateAccount")
+const dateCreateAccount = document.getElementById("dateCreateAccount")
+const buttonCreateAccount = document.getElementById("butonCreateAccount")
+
+buttonCreateAccount.addEventListener('click', (e) =>{
+    e.preventDefault()
+    if(nombreCreateAccount.value.length<1){
+        alert("Ingrese los datos correspondientes en la casilla de nombre")
+    }else if(userCreateAccount.value.length<1){
+        alert("Ingrese los datos correspondientes en la casilla de usuario")
+    }else if(emailCreateAccount.value.length<1){
+        alert("Ingrese los datos correspondientes en la casilla de correo electronico")
+    }else if(passwordCreateAccount.value.length<1){
+        alert("Ingrese los datos correspondientes en la casilla de contraseña")
+    } else {
+        window.location="Login.html";
+    }
+}) */
